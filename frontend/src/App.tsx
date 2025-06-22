f04f6e12acc71a9e9b036c4e8bbd546537d50564
Jordan-S1 <jordanshodipo1@gmail.com>
2025-06-22 01:26:36 +0100

    updated files

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,11 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-//import "./App.css";
+import { Button } from "@/components/ui/button";
 
-export default function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+function App() {
+  return (
+    <div className="flex min-h-svh flex-col items-center justify-center">
+      <Button>Click me</Button>
+    </div>
+  );
 }
+
+export default App;
